2e598a2dfa4101554b41e38c29d4721984dc4fb7
Luiz Picolo <luizpicolo@gmail.com>
2018-09-19 00:41:06 -0400

    Atualizado

--- main.rb
+++ main.rb
@@ -7,10 +7,8 @@ require_relative "biblioteca"
 livro = Livro.new "TDD by Picolo 2", "Técnico", "Luiz Picolo", 123456
 Biblioteca.salvar(livro)
 
-puts Biblioteca.ultimo_id
-
 livros = Biblioteca.selecionar('TDD by Picolo 2')
 
-# livros.each do |livro|
-#   puts livro.titulo
-# end
+livros.each do |livro|
+  puts livro.titulo
+end
