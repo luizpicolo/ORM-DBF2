a11320604d36fd37a19520a5d814cc9f8dcbf190
Luiz Picolo <luizpicolo@gmail.com>
2018-09-19 00:26:47 -0400

    Adicionado método para a criação dos DBs

--- livro.rb
+++ livro.rb
@@ -1,3 +1,5 @@
+require 'active_support/inflector'
+
 class Livro
   attr_accessor :id, :titulo, :categoria, :autor, :isbn
 
@@ -6,5 +8,12 @@ class Livro
     @categoria = categoria
     @autor = autor
     @isbn = isbn
+    @dir = "db/#{self.class.to_s.downcase.pluralize}"
+
+    criar_db
+  end
+
+  def criar_db
+    FileUtils::mkdir_p @dir unless File.directory?(@dir)
   end
 end
