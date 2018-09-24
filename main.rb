c5ea47f01f5ee3a9c510426c9daed1796735cb62
Luiz Picolo <luizpicolo@gmail.com>
2018-09-23 23:35:50 -0400

    update

--- main.rb
+++ main.rb
@@ -4,30 +4,14 @@ require_relative "file"
 require_relative "livro"
 require_relative "carro"
 
-Livro.criar(
-  titulo: 'TDDD',
+id =  Livro.criar(
+  titulo: 'Titulo',
   categoria: 'Técnico',
   autor: 'Sou eu',
   isbn: 123456
 )
 
-Carro.criar(
-  modelo: 'TDDD'
-)
+livro = Livro.selecionar(1)
+livro.titulo = "Titulo 3"
 
-# livro = Livro.new "TDD by Picolo 2", "Técnico", "Luiz Picolo", 123456
-# Biblioteca.salvar(livro)
-#
-# livro.titulo = "TDD by Picolo 3"
-#
-# Biblioteca.atualizar(livro)
-
-# puts Biblioteca.ultimo_id
-#
-# livros = Biblioteca.selecionar('TDD by Picolo 2')
-
-# livros = Biblioteca.listar
-#
-# livros.each do |livro|
-#   puts livro.titulo
-# end
+Livro.atualizar(livro)
--- orm.rb
+++ orm.rb
@@ -1,62 +1,59 @@
 class ORM
-
-  # @@path = 'db/livros'
-
   def self.criar(*atributos)
-    id = verifica_id
+    id = proximo_id
     File.open("#{path}/#{id}.yml", "a") do |arquivo|
       obj = self.new atributos
       obj.id = id
       arquivo.puts YAML.dump obj
+      return id
     end
   end
 
   def self.listar
-    @livros = []
+    @objs = []
     $/ = '\n\n'
 
-    (1..quantidade_registros).each do |id|
-      path = "#{@@path}/#{id}.yml"
-      if File.exist?(path)
-        File.open(path, "r").each do |livro|
-          @livros << YAML.load(livro)
+    (1..ultimo_id).each do |id|
+      _path = "#{path}/#{id}.yml"
+      if File.exist?(_path)
+        File.open(_path, "r").each do |obj|
+          @objs << YAML.load(obj)
         end
       end
     end
 
-    @livros
+    @objs
   end
 
-  def self.selecionar(titulo)
-    @livros_selecionados = []
-    listar.each do |livro|
-      @livros_selecionados << livro if livro.titulo == titulo
+  def self.selecionar(id)
+    listar.each do |obj|
+      return obj if obj.id == id
     end
-
-    @livros_selecionados
   end
 
-  def self.atualizar(livro)
-    FileUtils.rm "db/livros/#{livro.id}.yml"
-    salvar(livro)
+  def self.atualizar(obj)
+    FileUtils.rm "#{path}/#{obj.id}.yml"
+    File.open("#{path}/#{obj.id}.yml", "a") do |arquivo|
+      arquivo.puts YAML.dump obj
+      return obj.id
+    end
   end
 
   private
 
-  def self.verifica_id
-    # self.id.nil? ? proximo_id : self.id
-    proximo_id
-  end
+  # def self.verifica_id(atributos)
+  #   self.id.nil? ? proximo_id : self.id
+  # end
 
   def self.quantidade_registros
-    Dir.glob("db/livros/*.yml").size + 1
+    Dir.glob("#{path}/*.yml").size + 1
   end
 
   def self.ultimo_id
     files = Dir.entries("#{path}").sort_by do |file|
       File.ctime("#{path}/#{file}")
     end
-    File.basename "#{path}/#{files.last}", ".yml"
+    File.basename("#{path}/#{files.last}", ".yml").to_i
   end
 
   def self.path
